56b8fcbccdd7fcd8c0832bf013f0c672fe7c0ec2
Rasmus <Rasmuspalm@fastmail.se>
2024-06-02 14:48:45 +0200

    Feature Correct Setup * Added a gitignore * Added some setup information to the readMe * Installed correctly flask * Made a basic setup for the python code * Created a package requirements file

--- backend/README.md
+++ backend/README.md
@@ -10,3 +10,21 @@ npm run serve
 
 https://code.visualstudio.com/docs/python/tutorial-flask
 https://flask.palletsprojects.com/en/3.0.x/
+
+# Setup
+
+Create and activate a virtual environment:
+Unix, Linux, macOS:
+`python -m venv venv`
+`source venv/bin/activate`
+
+Windows Command Prompt:
+`python -m venv venv`
+`venv\Scripts\activate`
+
+Windows PowerShell:
+`python -m venv venv`
+`.\venv\Scripts\Activate`
+
+Install the dependencies:
+`pip install -r requirements.txt`
--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,46 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request # type: ignore
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Welcome to my fully backend password manager using inputs"
-
 # post
-@app.route("/manage", methods = ['POST'])
+@app.route("/")
 def actions():
     print("Hello")
     #  Ask user using inputs for:
     # If they wish to see current passwords
     # Create a new password
     # Manage passwords
+    # return "Welcome to my fully backend password manager using inputs"
 
-def currentPasswords():
-    print("Showing your current passwords")
+    return """
+    <h1>Welcome to my Python password manager CRUD application!</h1>
+    <p>Choose what you wish to do:</p>
+    <ul>
+        <li><a href="/create">Create a new password</a></li>
+        <li><a href="/view">View all passwords</a></li>
+        <li><a href="/update">Update the passwords</a></li>
+        <li><a href="/remove">Remove passwords</a></li>
+    </ul>
+    """
 
-def createNewPassword():
-    print("creating a new password")
+@app.route("/create")
+def createPasswords():
+    return "Here you can create passwords"
 
-def managePasswords():
-    print("Managing passwords")
+@app.route("/view")
+def viewPasswords():
+    return "Here you can view your passwords"
+
+@app.route("/update")
+def updatePasswords():
+    return "Here you can update your passwords"
     # Edit passwords
     # Create passwords
     # Remove passwords
     # Showing passwords
 
+@app.route("/remove")
+def removePasswords():
+    return "Here you can delete your passwords"
+
+if __name__ == '__main__':
+    app.run(debug=True)
